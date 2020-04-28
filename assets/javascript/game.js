cc5a7b228251223f65a4fb23a6d49272d86f4b73
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-04-28 16:40:40 -0400

    Created arrays and objects for the random number the user will have to guess and the 4 crystal values (points). Radomized the number the user guessed and the crystal points. Set up preliminary HTML & CSS setup using bootstrap.

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+
+
+
+// Array of numbers that user has to get by clicking cystals 
+var numGuess = [19, 20, 21, 22, 23, 24, 25, 26, 2, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 18, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120]
+
+//Point values that will be assigned to crystals 
+var numCrystals = {
+    crystalOne: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    crystalTwo: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    crystalThree: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    crystalFour: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+
+}
+
+
+// Randomizes the array of numbers the user has to guess
+var randomNums = numGuess[Math.floor(Math.random() * numGuess.length)];
+console.log(randomNums)
+
+var randomCrystalOne = numCrystals.crystalOne[Math.floor(Math.random() * numCrystals.crystalOne.length)];
+console.log(randomCrystalOne)
+
+var randomCrystalTwo = numCrystals.crystalTwo[Math.floor(Math.random() * numCrystals.crystalTwo.length)];
+console.log(randomCrystalTwo)
+
+var randomCrystalThree = numCrystals.crystalThree[Math.floor(Math.random() * numCrystals.crystalThree.length)];
+console.log(randomCrystalThree)
+
+var randomCrystalFour = numCrystals.crystalFour[Math.floor(Math.random() * numCrystals.crystalFour.length)];
+console.log(randomCrystalFour)
