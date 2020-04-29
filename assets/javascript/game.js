07436f6434db130780e7fd8335c5412af09fab1e
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-04-29 18:05:47 -0400

    Added structre to the HTML including the crystal images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,4 @@
 
-
-
 // Array of numbers that user has to get by clicking cystals 
 var numGuess = [19, 20, 21, 22, 23, 24, 25, 26, 2, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 18, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120]
 
@@ -16,16 +14,30 @@ var numCrystals = {
 
 // Randomizes the array of numbers the user has to guess
 var randomNums = numGuess[Math.floor(Math.random() * numGuess.length)];
-console.log(randomNums)
+// console.log(randomNums)
 
+
+// Randomizes the point values for each crystal 
 var randomCrystalOne = numCrystals.crystalOne[Math.floor(Math.random() * numCrystals.crystalOne.length)];
-console.log(randomCrystalOne)
+// console.log(randomCrystalOne)
 
 var randomCrystalTwo = numCrystals.crystalTwo[Math.floor(Math.random() * numCrystals.crystalTwo.length)];
-console.log(randomCrystalTwo)
+// console.log(randomCrystalTwo)
 
 var randomCrystalThree = numCrystals.crystalThree[Math.floor(Math.random() * numCrystals.crystalThree.length)];
-console.log(randomCrystalThree)
+// console.log(randomCrystalThree)
 
 var randomCrystalFour = numCrystals.crystalFour[Math.floor(Math.random() * numCrystals.crystalFour.length)];
-console.log(randomCrystalFour)
+// console.log(randomCrystalFour)
+
+
+
+
+
+
+// yellow
+
+// greennn 
+
+// bluee 
+
